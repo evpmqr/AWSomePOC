b3089b81acf0b214fa34df34832b1355ca9e8c4f
evpmqr <vuexx427@umn.edu>
2020-03-10 17:27:05 -0500

    documented workflow in main

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ func main() {
 	fileClient := file.NewClient()
 	builder := resources.NewBuilder(fileClient)
 
+	// Gets config file
 	reader, err := fileClient.GetFile("testing.yml")
 	if err != nil {
 		fmt.Errorf("error getting file reader: %s", err)
 	}
 
+	// Turns config file into a Template struct
 	template, err := builder.Unmarshal(reader)
 	if err != nil {
 		fmt.Errorf("error unmarshaling config: %s", err)
 	}
 
+	// Converts Template struct to a File
 	err = builder.ToTemplateFile(template)
 	if err != nil {
 		fmt.Errorf("error building template file: %s", err)
